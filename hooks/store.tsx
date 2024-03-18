7d1eaf205699b9ec99fecc3df9d666a05770b48d
HaseebAliKhan <57507467+HaseebAliKhan@users.noreply.github.com>
2024-03-19 01:29:07 +0500

    Zustand Boiler Plate

---
+++ hooks/store.tsx
@@ -0,0 +1,19 @@
+import { create } from 'zustand'
+
+interface BearStore {
+    bears: number
+    increasePopulation: () => void
+    removeAllBears: () => void
+}
+
+
+
+
+const useBearStore = create<BearStore>((set) => ({
+  bears: 0,
+  increasePopulation: () => set((state ) => ({ bears: state.bears + 1 })),
+  removeAllBears: () => set({ bears: 0 }),
+}))
+
+
+export default useBearStore
